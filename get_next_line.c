4f3e113605d606f5c5fc055ddaee40fd83fbf41f
Santi Legaristi Royo <slegaris@c1r13s2.42madrid.com>
2023-04-27 03:55:00 +0200

    Updates

--- get_next_line.c
+++ get_next_line.c
@@ -5,17 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: slegaris <slegaris@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/04 13:51:55 by slegaris          #+#    #+#             */
-/*   Updated: 2023/04/04 13:51:59 by slegaris         ###   ########.fr       */
+/*   Created: 2023/04/07 17:51:16 by slegaris          #+#    #+#             */
+/*   Updated: 2023/04/25 20:41:17 by slegaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define BUFFER_SIZE 1
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
 
-char	get_next_line(int fd)
+char	*ft_read_to_left_str(int fd, char *left_str)
 {
-	char buffer[BUFFER_SIZE];
-	n = read(fd, buffer, BUFFER_SIZE);
+	char	*buff;
+	int		rd_bytes;
 
-	ft_memcpy
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+			return (free(buff), free(left_str), NULL);
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
+}
+
+// Main that tests the speed an cycles
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
+
+    // Add these declarations and initializations
+    clock_t start_c, end_c;
+    double cpu_time_used;
+    struct timeval start_w, end_w;
+    double wall_time_used;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <file_name>\n", argv[0]);
+        return (1);
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+
+    // Record the start time
+    start_c = clock();
+    gettimeofday(&start_w, NULL);
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    // Record the end time
+    end_c = clock();
+    gettimeofday(&end_w, NULL);
+
+    // Calculate and print the total cycles and elapsed time
+    cpu_time_used = ((double) (end_c - start_c)) / CLOCKS_PER_SEC;
+    wall_time_used = (end_w.tv_sec - start_w.tv_sec) * 1000.0; // Convert to milliseconds
+    wall_time_used += (end_w.tv_usec - start_w.tv_usec) / 1000.0; // Add microseconds
+
+    printf("\nTotal cycles: %ld\n", (end_c - start_c));
+    printf("CPU time used: %.3fs\n", cpu_time_used);
+    printf("Wall time used: %.3fms\n", wall_time_used);
+
+    close(fd);
+    return (0);
 }
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slegaris <slegaris@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 17:51:39 by slegaris          #+#    #+#             */
+/*   Updated: 2023/04/25 19:16:59 by slegaris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+
+#endif
